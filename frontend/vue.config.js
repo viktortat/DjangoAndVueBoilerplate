87d370f9dfa1c2b2998da7aec50f23e1da4f5931
Lukáš Fuchs <panzelva@gmail.com>
2018-05-29 07:14:04 +0200

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,9 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:8000/',
-        ws: true,
-        changeOrigin: true
+        target: 'http://127.0.0.1:8000/'
       }
     }
   }
